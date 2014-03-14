5837a50c6f55768c05682c29bd12830c5fcbf9ce
endortrails <endortrails@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-14 05:19:23 +0000

    = 1.8 = * Feature: UI changes for responsive/mobile support * Feature: Tax suport * Feature: Added 3 shipping methods for prints * Tweak: sellMediaCart js now powers cart * Tweak: Removed $_SESSION cart * Tweak: Validate prices before sending to payment gateway * Tweak: Standardize data storage for payment meta _sell_media_payment_meta * Tweak: Responsive grids * Tweak: Checkout page reworked * Tweak: Improved emails * Tweak: New classes for all major parts of plugin * Tweak: Derive buyer data from payment gateway * Tweak: Publish paid purchases only * Tweak: Translations updates * Bug: Ajax setup * Bug: Pending payments * Bug: Conflict with JetPack infinite scroll
    git-svn-id: https://plugins.svn.wordpress.org/sell-media/tags/1.8@875227 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/deprecated.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Deprecated functions
+ */
+function sell_media_item_has_taxonomy_terms( $post_id=null, $taxonomy=null ) {
+    return false;
+}
+function sell_media_country_list( $current=null, $req=false ){
+    return false;
+}
+function sell_media_us_states_list( $current=null, $req=false ){
+    return false;
+}
+function sell_media_item_min_price( $post_id=null, $echo=true, $key='price' ){
+
+    $p = new SellMediaProducts;
+    $price = $p->get_lowest_price( $post_id );
+
+    if ( $echo ){
+        echo $price;
+    } else {
+        return $price;
+    }
+}
