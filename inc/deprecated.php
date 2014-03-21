d69d299b8eedc2225d765a250ddd4a4e310c0b65
endortrails <endortrails@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-21 03:24:49 +0000

    = 1.8.5 = * Feature: Packages feature added * Tweak: Make Sell_Media a singleton * Tweak: Add support for additional mime types * Tweak: Remove redundant calls to self::upload_dir, use one function now * Tweak: Hide file dimensions on cart if not image * Tweak: New file download method * Bug: Bulk uploads fix * Bug: PayPal tax * Bug: Deprecated functions missing for image caption
    git-svn-id: https://plugins.svn.wordpress.org/sell-media/tags/1.8.5@879230 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/deprecated.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Deprecated functions
+ */
+function sell_media_item_has_taxonomy_terms( $post_id=null, $taxonomy=null ) {
+    return false;
+}
+function sell_media_country_list( $current=null, $req=false ){
+    return false;
+}
+function sell_media_countries_list(){
+    return false;
+}
+function sell_media_us_states_list( $current=null, $req=false ){
+    return false;
+}
+function sell_media_item_min_price( $post_id=null, $echo=true, $key='price' ){
+
+    $price = Sell_Media()->products->get_lowest_price( $post_id );
+
+    if ( $echo ){
+        echo $price;
+    } else {
+        return $price;
+    }
+}
