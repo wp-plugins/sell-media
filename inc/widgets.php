9a83f5546fda37d828ebb3dd43c1a86ea0e35467
endortrails <endortrails@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-28 00:52:47 +0000

    = 1.8.7 = * Feature: Advanced Search integrated into core * Tweak: Remove filtering core WP search, just rely on Advanced Search now * Bug: number_format() warning when no default price set in admin
    git-svn-id: https://plugins.svn.wordpress.org/sell-media/tags/1.8.7@883299 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/widgets.php
@@ -0,0 +1,46 @@
+<?php
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Register Sidebar
+ */
+
+function sell_media_widgets_register_sidebar() {
+
+	register_sidebar( array(
+		'name' => __( 'Sell Media Below Single Item', 'sell_media' ),
+		'id' => 'sell-media-single-sidebar',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'sell_media_widgets_register_sidebar' );
+
+/**
+* Include Widgets
+*/
+require_once( SELL_MEDIA_PLUGIN_DIR . '/inc/widgets/sell-media-recent.php' );
+require_once( SELL_MEDIA_PLUGIN_DIR . '/inc/widgets/sell-media-similar.php' );
+require_once( SELL_MEDIA_PLUGIN_DIR . '/inc/widgets/sell-media-featured.php' );
+require_once( SELL_MEDIA_PLUGIN_DIR . '/inc/widgets/sell-media-keywords.php' );
+
+/**
+ * Display Sidebar
+ */
+function sell_media_widgets_single_sidebar() { ?>
+
+	<?php if ( is_active_sidebar( 'sell-media-single-sidebar' ) ) : ?>
+		<section id="sell-media-single-sidebar" class="widget-area" role="complementary">
+			<?php dynamic_sidebar( 'sell-media-single-sidebar' ); ?>
+		</section>
+	<?php endif; ?>
+
+<?php
+
+}
+add_action( 'sell_media_single_bottom_hook', 'sell_media_widgets_single_sidebar' );
