728441758b0fe36193b479036ef294fbbd88deca
endortrails <endortrails@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-29 19:47:22 +0000

    = 2.0.3 = * New Feature: Check for newest version of Sell Media extensions. * Tweak: Add search by ID in admin. * Tweak: Don't send empty sizes and licenses to gateway. * Tweak: Improved html email receipts. * Bugfix: Missing sell_media_image class on gallery images. * Bugfix: Move non-image files to /uploads/sell_media/. * Bugfix: Filepath checks for non-image uploads (zips, etc).
    git-svn-id: https://plugins.svn.wordpress.org/sell-media/tags/2.0.3@1149432 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/gateways/paypal.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+ * Retrieve the correct PayPal Redirect based on http/s
+ * and "live" or "test" mode, i.e., sandbox.
+ *
+ * @return PayPal URI
+ */
+function sell_media_get_paypal_redirect( $ssl_check=false ) {
+
+    if ( is_ssl() || ! $ssl_check ) {
+        $protocol = 'https://';
+    } else {
+        $protocol = 'http://';
+    }
+
+    if ( sell_media_test_mode() ) {
+        $paypal_uri = $protocol . 'www.sandbox.paypal.com/cgi-bin/webscr';
+    } else {
+        $paypal_uri = $protocol . 'www.paypal.com/cgi-bin/webscr';
+    }
+
+    return $paypal_uri;
+}
+
+
+/**
+ * Listen for a $_GET request from our PayPal IPN.
+ * This would also do the "set-up" for an "alternate purchase verification"
+ */
+function sell_media_listen_for_paypal_ipn() {
+    if ( isset( $_GET['sell_media-listener'] )
+        && $_GET['sell_media-listener'] == 'IPN'
+        || isset( $_GET['test'] )
+        && $_GET['test'] == true ) {
+        do_action( 'sell_media_verify_paypal_ipn' );
+    }
+}
+add_action( 'init', 'sell_media_listen_for_paypal_ipn' );
+
+
+/**
+ * When a payment is made PayPal will send us a response and this function is
+ * called. From here we will confirm arguments that we sent to PayPal which
+ * the ones PayPal is sending back to us.
+ * This is the Pink Lilly of the whole operation.
+ */
+function sell_media_process_paypal_ipn() {
+
+    /**
+     * Instantiate the IPNListener class
+     */
+    include( dirname( __FILE__ ) . '/php-paypal-ipn/IPNListener.php' );
+    $listener = new IPNListener();
+
+    /**
+     * Set to PayPal sandbox or live mode
+     */
+    $settings = sell_media_get_plugin_options();
+    $listener->use_sandbox = ( $settings->test_mode ) ? true : false;
+
+    /**
+     * Check if IPN was successfully processed
+     */
+    if ( $verified = $listener->processIpn() ) {
+
+        /**
+         * Log successful purchases
+         */
+        $transactionData = $listener->getPostData(); // POST data array
+        file_put_contents( 'ipn_success.log', print_r( $transactionData, true ) . PHP_EOL, LOCK_EX | FILE_APPEND );
+
+        $message = null;
+
+        /**
+         * Verify seller PayPal email with PayPal email in settings
+         *
+         * Check if the seller email that was processed by the IPN matches what is saved as
+         * the seller email in our DB
+         */
+        $settings = sell_media_get_plugin_options();
+        if ( $_POST['receiver_email'] != $settings->paypal_email ){
+            $message .= "\nEmail seller email does not match email in settings\n";
+        }
+
+        /**
+         * Verify currency
+         *
+         * Check if the currency that was processed by the IPN matches what is saved as
+         * the currency setting
+         */
+        $settings = sell_media_get_plugin_options();
+        if ( $_POST['mc_currency'] != $settings->currency ){
+            $message .= "\nCurrency does not match those assigned in settings\n";
+        }
+
+        /**
+         * Check if this payment was already processed
+         *
+         * PayPal transaction id (txn_id) is stored in the database, we check
+         * that against the txn_id returned.
+         */
+        $txn_id = get_post_meta( $_POST['custom'], 'txn_id', true );
+        if ( empty( $txn_id ) ){
+            update_post_meta( $_POST['custom'], 'txn_id', $_POST['txn_id'] );
+        } else {
+            $message .= "\nThis payment was already processed\n";
+        }
+
+        /**
+         * Verify the payment is set to "Completed".
+         *
+         * Create a new payment, send customer an email and empty the cart
+         */
+        if ( ! empty( $_POST['payment_status'] ) && $_POST['payment_status'] == 'Completed' ){
+
+            $data = array(
+                'post_title'    => $_POST['payer_email'],
+                'post_status'   => 'publish',
+                'post_type'     => 'sell_media_payment'
+            );
+
+            $payment_id = wp_insert_post( $data );
+            $payments = Sell_Media()->payments;
+
+            if ( $payment_id ) {
+
+                update_post_meta( $payment_id, '_paypal_args', $_POST );
+
+                // record the PayPal payment details
+                $payments->paypal_copy_args( $payment_id );
+
+                // create new user, auto log them in, email them registration
+                Sell_Media()->customer->insert( $_POST['payer_email'], $_POST['first_name'], $_POST['last_name'] );
+
+                $message .= "\nSuccess! Your purchase has been completed.\n";
+                $message .= "Your transaction number is: {$_POST['txn_id']}\n";
+                $message .= "To email: {$_POST['payer_email']}\n";
+
+                // Send email to buyer and admin
+                $email_status = $payments->email_receipt( $payment_id, $_POST['payer_email'] );
+                $admin_email_status = $payments->email_receipt( $payment_id, $settings->from_email );
+
+                $message .= "{$email_status}\n";
+                $message .= "{$admin_email_status}\n";
+
+                do_action( 'sell_media_after_successful_payment', $payment_id );
+
+            }
+
+        } else {
+
+            $message .= "\nPayment status not set to Completed\n";
+
+        }
+
+        /**
+         * Check if this is the test mode
+         *
+         * If this is the test mode we email the IPN text report.
+         * note about and box http://stackoverflow.com/questions/4298117/paypal-ipn-always-return-payment-status-pending-on-sandbox
+         */
+        if ( $settings->test_mode == true ){
+
+            $message .= "\nTest Mode\n";
+            $email = array(
+                'to' => $settings->from_email,
+                'subject' => 'Verified IPN',
+                'message' =>  $message . "\n" . $listener->getTextReport()
+                );
+
+            wp_mail( $email['to'], $email['subject'], $email['message'] );
+
+        }
+
+    } else {
+
+        /**
+         * Log errors
+         */
+        $errors = $listener->getErrors();
+        file_put_contents( 'ipn_errors.log', print_r( $errors, true ) . PHP_EOL, LOCK_EX | FILE_APPEND );
+
+        /**
+         * An Invalid IPN *may* be caused by a fraudulent transaction attempt. It's
+         * a good idea to have a developer or sys admin manually investigate any
+         * invalid IPN.
+         */
+        wp_mail( $settings->from_email, 'Invalid IPN', $listener->getTextReport() );
+
+    }
+}
+add_action( 'sell_media_verify_paypal_ipn', 'sell_media_process_paypal_ipn' );
---
+++ js/admin-items.js
@@ -0,0 +1,206 @@
+jQuery( document ).ready(function( $ ){
+
+    /**
+     * Uploading files
+     */
+    var file_frame;
+
+    /**
+     * Upload button clicked
+     * Used on add/edit item page
+     * Ajax callback to show uploaded/selected items in meta box
+     * Updates hidden input field with new attachment ids
+     */
+    $(document).on('click', '.sell-media-upload-button', function( event ){
+
+        event.preventDefault();
+
+        var post_id = $(this).data('id');
+
+        if ( file_frame ) {
+            file_frame.open();
+            return;
+        }
+
+        // Create the media frame.
+        file_frame = wp.media.frames.file_frame = wp.media({
+            title: 'Select Images To Sell',
+            description: 'This is the description',
+            button: {
+              text: 'Sell All Selected Images',
+            },
+            multiple: 'add'  // Set to true to allow multiple files to be selected
+        });
+
+        // When an image is selected, run a callback.
+        file_frame.on( 'select', function() {
+
+            var attachments = file_frame.state().get('selection').toJSON();
+
+            /**
+             * Since we only want id, title, description and url, we build a new JSON object
+             * the current one (attachments) is bloated and causing the bulk updater to fail
+             * after ~23 items
+             */
+            var attachments_array = [];
+
+            $.each( attachments, function( i, item ){
+                attachments_array.push({
+                    id: item.id,
+                    title: item.title,
+                    description: item.description,
+                    url: item.url
+                });
+            });
+
+            var data = {
+                action: "sell_media_upload_callback",
+                attachments: attachments_array,
+                id: post_id,
+                security: $('#sell_media_meta_box_nonce').val()
+            };
+
+            //$('.sell-media-upload-list').empty();
+            $('.sell-media-ajax-loader').show();
+
+            $.ajax({
+                type: "POST",
+                url: ajaxurl,
+                data: data,
+                success: function( msg ){
+                    $('.sell-media-ajax-loader').hide();
+                    $('.sell-media-upload-list').append( msg );
+                    update_files();
+                }
+            });
+        });
+
+        // Finally, open the modal
+        file_frame.open();
+    });
+
+    /**
+     * Remove from file list
+     */
+    $('.sell-media-delete').on('click', function( event ){
+        event.preventDefault();
+
+        // Remove the file
+        var id = $(this).data('id');
+        $('.sell-media-attachment[data-post_id="' + id +'"]').remove();
+
+        // Update the file list hidden field
+        update_files();
+
+    });
+
+    /**
+     * Update the file list hidden field
+     */
+    function update_files(){
+        var array = [];
+        $('.sell-media-upload-list li').each(function(){
+            array.push($(this).data('post_id'));
+        });
+        var new_array = array.join(',');
+
+        // Set the new value
+        $('#sell-media-attachment-id').val(new_array);
+    }
+
+    /**
+     * Toggle the upload options
+     * Used on add/edit item page
+     */
+    $('.sell-media-upload-options').on('click', function( event ){
+        event.preventDefault();
+        $(this).find('span').toggleClass('dashicons-arrow-up dashicons-arrow-down');
+        $('#sell-media-upload-show-options').toggle();
+    });
+
+    /**
+     * Remove disabled property when bulk selector changes
+     * Used on add/edit item page
+     */
+    $('#sell-media-upload-bulk-selector').change(function() {
+        var button = $('#sell-media-upload-bulk-processor');
+        if ($(this).val()) {
+            $(button).prop('disabled', false);
+        } else {
+            $(button).prop('disabled', true);
+        }
+    });
+
+    /**
+     * Ajax callback to insert attachments in bulk upload directory into WP
+     * Used on add/edit item page
+     */
+    $('#sell-media-upload-bulk-processor').on('click', function( event ){
+        event.preventDefault();
+
+        var selector = $(this);
+
+        $(selector).text($(selector).data('uploading-text'));
+
+        var directory = $('#sell-media-upload-bulk-selector').val(),
+            post_id = $('.sell-media-upload-button').data('id');
+
+        var data = {
+                action: "sell_media_upload_bulk_callback",
+                dir: directory,
+                id: post_id,
+                security: $('#sell_media_meta_box_nonce').val()
+            };
+
+            $.ajax({
+                type: "POST",
+                url: ajaxurl,
+                data: data,
+                success: function( msg ){
+                    $('.sell-media-upload-list').append( msg );
+                    update_files();
+                    $(selector).text($(selector).data('default-text'));
+                    //console.log(msg);
+                }
+            });
+
+    });
+
+    /**
+     * Upload thumbnail icon for collections
+     */
+    $(document).on('click', '.sell-media-upload-trigger-collection-icon', function( event ){
+
+        event.preventDefault();
+
+        if ( file_frame ) {
+            file_frame.open();
+            return;
+        }
+
+        // Create the media frame.
+        file_frame = wp.media.frames.file_frame = wp.media({
+            title: 'Select Images To Sell',
+            description: 'This is the description',
+            button: {
+              text: 'Use selected image as icon',
+            },
+            multiple: false  // Set to true to allow multiple files to be selected
+        });
+
+        // When an image is selected, run a callback.
+        file_frame.on( 'select', function() {
+
+            // We set multiple to false so only get one image from the uploader
+            var attachment = file_frame.state().get('selection').first().toJSON();
+            $('#collection_icon_input_field').val( attachment.id );
+            $('#collection_icon_url').val( attachment.url );
+            $('#collection_icon_target').html( '<img src="'+attachment.sizes.thumbnail.url+'" /><br><a href="javascript:void(0);" class="upload_image_remove">Remove</a>' );
+
+        });
+
+        // Finally, open the modal
+        file_frame.open();
+    });
+
+});
